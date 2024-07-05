3be9863616c98b08fcc532c9e96af49ec073c9ff
Aniket <aniketnr5023@gmail.com>
2024-07-06 00:19:41 +0530

    Added Dayone File

---
+++ Day1/src/com/Day1/task_One/task_One.java
@@ -0,0 +1,20 @@
+package com.Day1.task_One;
+
+public class task_One {
+
+	public static void main(String[] args) {
+		
+		// Area of parallelogram 
+		float base = 43.8990f;
+		float height = 96.5895f;
+		
+		float areaOfParallelogram = base * height;
+		double areaOfParallelogram1 = base * height;
+		System.out.println("Area of parallelogram : " + areaOfParallelogram);
+		System.out.println("Area of parallelogram : " + areaOfParallelogram1);
+		
+		
+
+	}
+
+}
